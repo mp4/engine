e9207ee8bd0bfd29337ff100d0aa65fa4370a2b3
Sandro Koll <sandro.koll@x-cellent.com>
2018-12-28 13:54:30 +0100

    Set split to its minimum size if user right presses on spacer

--- gui/splitter.go
+++ gui/splitter.go
@@ -246,17 +246,16 @@ func (s *Splitter) onMouse(evname string, ev interface{}) {
 			}
 			s.root.SetMouseFocus(&s.spacer)
 		} else if mev.Button == window.MouseButtonRight {
+			s.root.SetCursorNormal()
 			s.rightPressed = true
+			s.SetSplit(float32(s.min0))
 		}
 	case OnMouseUp:
 		if mev.Button == window.MouseButtonLeft {
 			s.root.SetCursorNormal()
 			s.leftPressed = false
 			s.root.SetMouseFocus(nil)
-		} else if mev.Button == window.MouseButtonRight && s.rightPressed {
-			if s.mouseOver {
-				s.SetSplit(float32(s.min0))
-			}
+		} else if mev.Button == window.MouseButtonRight {
 			s.rightPressed = false
 		}
 	default:
